5dbd981138bb69ce4d3194c6446e0c498afd82af
Easton Potokar <contagon6@gmail.com>
2023-03-28 16:56:42 -0400

    Added arm plotting

--- cov/cov_prop.py
+++ cov/cov_prop.py
@@ -1,7 +1,18 @@
 from arm import ArmSE3
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.linalg import expm
+
+def plot_arm(theta, l, ax):
+    x = [0]
+    y = [0]
+    print(theta)
+    x.append(l*np.cos(theta[0]))
+    y.append(l*np.sin(theta[0]))
+
+    x.append(x[-1] + l*np.cos(theta[0]+theta[1]))
+    y.append(y[-1] + l*np.sin(theta[0]+theta[1]))
+
+    ax.plot(x, y, marker='o', c='r')
 
 # ------------------------- Setup single 2link planar arm ------------------------- #
 """
@@ -9,10 +20,10 @@ Arm is defined with x-axis right, y-axis up, z-axis out of page
 2 link arm with one link at origin, another distance l away
 Both links rotate about positive z-axis
 """
-zero = np.eye(4)
-zero[:3,3] = [2,0,0]
-
 l = 1
+zero = np.eye(4)
+zero[:3,3] = [2*l,0,0]
+
 w = np.array([
     [0,0,1],
     [0,0,1]
@@ -68,21 +79,24 @@ fk_noisy_prop = [fk_true@arm._exp(w) for w in noise]
 
 
 # ------------------------- Plot resulting trajectory ------------------------- #
-fig, ax = plt.subplots(1, 2)
+fig, ax = plt.subplots(1, 2, figsize=(7,3))
 
 ax[0].set_title("Actual Distribution")
 x = np.array([i[0,3] for i in fk_noisy] )
 y = np.array([i[1,3] for i in fk_noisy] )
 ax[0].scatter(x, y, s=0.5, alpha=0.5)
+plot_arm(theta, l, ax[0])
 
-ax[1].set_title("Sampled Distribution")
+ax[1].set_title("Approximated Distribution")
 x = np.array([i[0,3] for i in fk_noisy_prop] )
 y = np.array([i[1,3] for i in fk_noisy_prop] )
 ax[1].scatter(x, y, s=0.5, alpha=0.5)
+plot_arm(theta, l, ax[1])
 
 ax[0].set_aspect('equal')
 ax[1].set_aspect('equal')
 
 plt.setp(ax, xlim=ax[0].get_xlim(), ylim=ax[0].get_ylim())
 plt.tight_layout()
+plt.savefig("arm_cov_prop.png")
 plt.show()
